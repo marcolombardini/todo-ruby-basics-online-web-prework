7e6d074d39f3737e89bef80d187c680d720e7ad9
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-02 15:21:27 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,44 @@
-def division(num1, num2)
- 
+def division(num1 = (42), num2 = (7))
+  
+  sum = num1 / num2
+  
+sum
 end
 
-def assign_variable(value)
+def assign_variable(value = "bob")
+  value
   
 end
 
-def argue
+def argue(greeting)
+  
+  puts "I'm right and you are wrong!"
+  "I'm right and you are wrong!"
   
 end
 
-def greeting
+def greeting(greeting, name)
+  
+  "#{greeting} #{name}"
 
 end
 
 def return_a_value
+  
+  puts "Nice"
+  "Nice"
 
 end
 
 def last_evaluated_value
+  
+  "expert"
 
 end
 
-def pizza_party
+def pizza_party(cheese = "cheese")
+  
+  pepperoni = "pepperoni"
+  cheese
 
 end
